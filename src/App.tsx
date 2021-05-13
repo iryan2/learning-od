c52c7f661cc2a2eb513346107f61d8eeb102bf3f
Ian Ryan <iryan2@gmail.com>
2021-05-13 13:39:33 +1000

    Add greeting message

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import config from './config.json'
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,16 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {config.Greeting}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
