eddb3c4678ebd897da18ce6cec8758ffcd64d5aa
Ian Ryan <iryan2@gmail.com>
2021-05-14 14:45:33 +1000

    Fetch config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import config from './config.json'
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import config from './config.json'
+import logo from "./logo.svg";
+import "./App.css";
+
+async function getConfig() {
+  const config = await fetch("config.json");
+  const data = await config.json();
+  console.log(`data`, data);
+  return data;
+}
 
 function App() {
+  const config = getConfig();
+  console.log(`config`, config);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {config.Greeting}
-        </p>
+        {/* @ts-ignore */}
+        <p>{config?.Greeting}</p>
       </header>
     </div>
   );
